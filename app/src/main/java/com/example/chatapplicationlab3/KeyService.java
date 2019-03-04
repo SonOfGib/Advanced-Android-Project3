5d8486bbfd80a8638f77335451a86d2f8a25e96a
SonOfGib <seanmoner@gmail.com>
2019-03-04 00:00:25 -0500

    Additional KeyService Code

--- app/src/main/java/com/example/chatapplicationlab3/KeyService.java
+++ app/src/main/java/com/example/chatapplicationlab3/KeyService.java
@@ -2,15 +2,161 @@ package com.example.chatapplicationlab3;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Binder;
 import android.os.IBinder;
+import android.preference.PreferenceManager;
+import android.util.Base64;
+import android.util.Log;
+
+import com.google.gson.Gson;
+
+import org.json.JSONObject;
+import java.lang.reflect.Type;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
+import java.util.HashMap;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
 
 public class KeyService extends Service {
+    KeyPairGenerator kpg;
+    KeyPair keys;
+    Cipher cipher;
+    BigInteger modulus;
+    HashMap<String,String> partnerKeys;
+    SharedPreferences prefs;
+    private final IBinder mBinder = new LocalBinder();
+    final String PARTNER_MAP_PREF = "PARTNER_MAP_PREF";
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        KeyService getService() {
+            return KeyService.this;
+        }
+    }
+
+
     public KeyService() {
+        partnerKeys = new HashMap<>();
+    }
+
+    KeyPair generateMyKeys(){
+        try {
+            kpg = KeyPairGenerator.getInstance("RSA");
+            keys = kpg.generateKeyPair();
+            KeyFactory fact = KeyFactory.getInstance("RSA");
+            cipher = Cipher.getInstance("RSA");
+            RSAPrivateKey privateKey = (RSAPrivateKey) keys.getPrivate();
+            RSAPublicKey publicKey = (RSAPublicKey) keys.getPublic();
+
+            String privateKeyString = privateKey.getPrivateExponent().toString();
+            String publicKeyString = publicKey.getPublicExponent().toString();
+
+            Log.d("Public", publicKeyString);
+            Log.d("Private", privateKeyString);
+            Log.d("Mod", publicKey.getModulus().toString());
+            RSAPrivateKeySpec privKeySpec = new RSAPrivateKeySpec(privateKey.getModulus(), new BigInteger(privateKeyString));
+            privateKey = (RSAPrivateKey) fact.generatePrivate(privKeySpec);
+            RSAPublicKeySpec pubKeySpec = new RSAPublicKeySpec(publicKey.getModulus(), new BigInteger(publicKeyString));
+            modulus = publicKey.getModulus();
+            publicKey = (RSAPublicKey) fact.generatePublic(pubKeySpec);
+
+            return keys;
+        }catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    void storePublicKey(String partnerName, String publicKey){
+        partnerKeys.put(partnerName,publicKey);
+        //Save the map to preferences.
+        String mapString = new JSONObject(partnerKeys).toString();
+        prefs.edit().putString(PARTNER_MAP_PREF, mapString).apply();
+    }
+
+    RSAPublicKey getPublicKey(String partnerName){
+        String pubkey = (String) partnerKeys.get(partnerName);
+        RSAPublicKeySpec pubKeySpec = new RSAPublicKeySpec(modulus,new BigInteger(pubkey));
+        try {
+            KeyFactory fact = KeyFactory.getInstance("RSA");
+            return (RSAPublicKey) fact.generatePublic(pubKeySpec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Encrypt with the partners public key.
+     * @param plainText The actual text.
+     * @param partnerName The partners name.
+     * @return The cipher text.
+     */
+    public String encrypt(String plainText, String partnerName){
+        RSAPublicKey publicKey = getPublicKey(partnerName);
+        try{
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+            byte[] encryptedText = cipher.doFinal(plainText.getBytes());
+            return Base64.encodeToString(encryptedText, Base64.DEFAULT);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * Decrypt with our private key.
+     * @param cipherText The encrypted text.
+     * @return The decrypted text.
+     */
+    public String decrypt(String cipherText){
+        byte[] encryptedText = Base64.decode(cipherText, Base64.DEFAULT);
+        try{
+            cipher.init(Cipher.DECRYPT_MODE, keys.getPrivate());
+            String decryptedText = new String(cipher.doFinal(encryptedText));
+            return decryptedText;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     @Override
+    public void onCreate() {
+        super.onCreate();
+        prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String jsonString = prefs.getString(PARTNER_MAP_PREF, "");
+        Gson gson = new Gson();
+        HashMap<String, String> map = new HashMap<>();
+        if(!jsonString.equals("")) {
+            map = (HashMap<String, String>) gson.fromJson(jsonString, map.getClass());
+            partnerKeys = map;
+        }
+    }
+
+
+
+    @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
--- app/src/main/java/com/example/chatapplicationlab3/MainActivity.java
+++ app/src/main/java/com/example/chatapplicationlab3/MainActivity.java
@@ -2,14 +2,17 @@ package com.example.chatapplicationlab3;
 
 import android.app.AlertDialog;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.content.SharedPreferences;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.NfcEvent;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,10 +33,18 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
     SharedPreferences prefs;
     final static String USER_PREF_KEY = "USER_PREF_KEY";
     final static String USER_EXTRA = "USER_EXTRA";
+
+    boolean mBounded = false;
+    KeyService mKeyService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent mIntent = new Intent(this, KeyService.class);
+        bindService(mIntent, mConnection, BIND_AUTO_CREATE);
+
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
 
@@ -49,7 +60,7 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
         viewSwap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(mContext,MessageActivity.class);
+                Intent intent = new Intent(mContext, MessageActivity.class);
                 intent.putExtra(USER_EXTRA, username);
                 startActivity(intent);
             }
@@ -65,22 +76,21 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
 
     }
 
-    private void updateUsername(){
+    private void updateUsername() {
         Button viewSwap = findViewById(R.id.sendModeButton);
         username = prefs.getString(USER_PREF_KEY, null);
-        if(username == null){
+        if (username == null) {
             viewSwap.setEnabled(false);
             Toast.makeText(this, "No username found, please set one!",
                     Toast.LENGTH_SHORT).show();
-        }
-        else{
+        } else {
             viewSwap.setEnabled(true);
             TextView usernameLabel = findViewById(R.id.usernameDisplayLabel);
             usernameLabel.setText(username);
         }
     }
 
-    private void buildUsernameDialog(){
+    private void buildUsernameDialog() {
         //Preparing views
         LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
         View layout = inflater.inflate(R.layout.dialog_layout, (ViewGroup) findViewById(R.id.layout_root));
@@ -95,8 +105,8 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
                 String localUsername = usernameBox.getText().toString();
-                boolean committed = prefs.edit().putString(USER_PREF_KEY,localUsername).commit();
-                if(committed){
+                boolean committed = prefs.edit().putString(USER_PREF_KEY, localUsername).commit();
+                if (committed) {
                     username = localUsername;
                 }
             }
@@ -121,7 +131,7 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
     protected void onResume() {
         super.onResume();
         updateUsername();
-        nfcAdapter.enableForegroundDispatch(this,mPendingIntent,null,null);
+        nfcAdapter.enableForegroundDispatch(this, mPendingIntent, null, null);
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             readPayload(getIntent());
         }
@@ -138,7 +148,7 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
                 ((NdefMessage) intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)[0])
                         .getRecords()[0]
                         .getPayload());
-        Toast.makeText(this,"Recieved NFC tag", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "Recieved NFC tag", Toast.LENGTH_LONG).show();
         TextView disp = findViewById(R.id.messageDisplay);
         disp.setText(payload);
     }
@@ -156,5 +166,29 @@ public class MainActivity extends AppCompatActivity implements NfcAdapter.Create
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
+    }
+
+    ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBounded = false;
+            mKeyService = null;
+        }
+
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBounded = true;
+            KeyService.LocalBinder mLocalBinder = (KeyService.LocalBinder) service;
+            mKeyService = mLocalBinder.getService();
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mBounded) {
+            unbindService(mConnection);
+            mBounded = false;
+        }
     }
 }
